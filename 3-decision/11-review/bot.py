460350f3f411bbaa0d894f5d76750249f2fa77a7
4mayet21 <4mayet21@solent.ac.uk>
2019-10-01 11:17:27 +0000

    Added code to review the use of a decisions, nesting and logical operators.

---
+++ 3-decision/11-review/bot.py
@@ -0,0 +1,7 @@
+print("wake me up...")
+lyrics = input()
+
+
+
+if ((lyrics == "inside") or (lyrics == "before you go go") or (lyrics == "when september ends") or (lyrics == "when its all over")):
+    print("this is no time for song lyrics")
