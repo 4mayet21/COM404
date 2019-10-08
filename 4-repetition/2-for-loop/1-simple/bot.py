a89f6737dab3d20058681b2dc103f23e0ca8873a
4mayet21 <4mayet21@solent.ac.uk>
2019-10-08 09:53:31 +0000

    Added code to demonstrate the use of a simple for loop./home/gitpod/.pyenv/versions/3.7.4/bin/python3

---
+++ 4-repetition/2-for-loop/1-simple/bot.py
@@ -0,0 +1,9 @@
+mtNum = int(input("how many mountains should I display"))
+
+for num in range(0, mtNum):
+    print("           __")
+    print("          /  \_  ")
+    print("         /^    \ ")
+    print("        /  ^    \_" )
+    print("      _/ ^ ^     ^\ " )
+    print("     /  ^     ^    \ ")
