fb7f4e64c6b803210865cd3292795b6c355b9c29
4mayet21 <4mayet21@solent.ac.uk>
2019-10-08 10:20:31 +0000

    Added code to demonstrate the use of for loops in string indexing.

---
+++ 4-repetition/2-for-loop/4-characters/bot.py
@@ -0,0 +1,5 @@
+scrawl = (input("What strange markings do you see?  "))
+index = 0
+for item in range(0, len(scrawl)):
+    print( "index " + str(index) + " : " + scrawl[item])
+    index = index + 1
