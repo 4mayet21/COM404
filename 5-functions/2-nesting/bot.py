abf08738eccc8635038e2f4dc01160e04eef7d83
4mayet21 <4mayet21@solent.ac.uk>
2019-10-15 08:59:11 +0000

    Added code to demonstrate use of  userDefined function w/ nestedDecision

---
+++ 5-functions/2-nesting/bot.py
@@ -0,0 +1,16 @@
+
+def identify():
+    sight = input("Legolas, what do your Elf eyes see? ")
+    if sight == "a large boulder":
+        print("its time to run")
+    elif sight == "They're taking the Hobbits to Isengard":
+        print("*stares intently*")
+    elif sight == "a Balrog of Morgoth":
+        print("Fly you fools!")
+    elif sight == "a large army":
+        print("Abandon your posts! Flee, Flee for your lives")
+    else:
+        print("we will be fine")
+
+
+identify()
